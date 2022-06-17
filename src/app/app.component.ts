5d37112ef8933223d223140b21460b658966f4c6
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-06-17 14:37:20 +0300

    bingo games are added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bens-bingo-app';
+  games = [1, 2, 3, 4];
 }
