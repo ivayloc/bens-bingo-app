e57c1148ce758f0b50727c7ffaa68a2e13ec0913
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-06-19 13:20:01 +0300

    transloco is added

---
+++ src/app/transloco-root.module.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  TRANSLOCO_LOADER,
+  Translation,
+  TranslocoLoader,
+  TRANSLOCO_CONFIG,
+  translocoConfig,
+  TranslocoModule,
+} from '@ngneat/transloco';
+import { Injectable, NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+import {
+  TranslocoPersistLangModule,
+  TRANSLOCO_PERSIST_LANG_STORAGE,
+} from '@ngneat/transloco-persist-lang';
+
+@Injectable({ providedIn: 'root' })
+export class TranslocoHttpLoader implements TranslocoLoader {
+  constructor(private http: HttpClient) {}
+
+  getTranslation(lang: string) {
+    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+  }
+}
+
+@NgModule({
+  imports: [
+    TranslocoPersistLangModule.forRoot({
+      storage: {
+        provide: TRANSLOCO_PERSIST_LANG_STORAGE,
+        useValue: localStorage,
+      },
+    }),
+  ],
+  exports: [TranslocoModule],
+  providers: [
+    {
+      provide: TRANSLOCO_CONFIG,
+      useValue: translocoConfig({
+        availableLangs: ['en'],
+        defaultLang: 'en',
+        // Remove this option if your application doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        prodMode: environment.production,
+      }),
+    },
+    { provide: TRANSLOCO_LOADER, useClass: TranslocoHttpLoader },
+  ],
+})
+export class TranslocoRootModule {}
