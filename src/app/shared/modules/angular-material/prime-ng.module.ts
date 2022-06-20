0e75ffa5b61d2072f38c1482e1ce3baffaaad90b
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-06-21 00:23:06 +0300

    carousel is partially added

--- src/app/shared/modules/angular-material/prime-ng.module.ts
+++ src/app/shared/modules/angular-material/prime-ng.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
-
+import { CarouselModule } from 'primeng/carousel';
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [ScrollPanelModule],
+  exports: [CarouselModule, ScrollPanelModule],
 })
 export class PrimeNgModule {}
