894e6d3aad197d8f4a17bfa920a0eb8bcb31682b
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-06-23 00:51:18 +0300

    ngrx is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { TranslocoRootModule } from './shared/modules/transloco-root.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +21,22 @@ import { SharedModule } from './shared/shared.module';
     TranslocoRootModule,
     BrowserAnimationsModule,
     SharedModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: false,
+      autoPause: true,
+      features: {
+        pause: false,
+        lock: true,
+        persist: true,
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
