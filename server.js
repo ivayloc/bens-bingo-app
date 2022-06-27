6d283e136dc930c2cf831ddcf76fe532ffcb6623
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-06-27 20:54:42 +0300

    compression module is removed

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const path = require("path");
-const compression = require("compression");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/bens-bingo-app"), compression());
+app.use(express.static(__dirname + "/dist/bens-bingo-app"));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/bens-bingo-app/index.html"));
