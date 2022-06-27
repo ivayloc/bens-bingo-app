798a60d274aa55edf4143d03025da8565c4c12a9
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-06-27 19:22:43 +0300

    gzip is added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
+const compression = require("compression");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/bens-bingo-app"));
+app.use(express.static(__dirname + "/dist/bens-bingo-app"), compression());
+
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/bens-bingo-app/index.html"));
 });
