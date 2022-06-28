842f9f5adc53e231dc9106d84a40f232ba69ae48
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-06-28 22:24:24 +0300

    paralax in progress

--- src/app/home/components/header-character/header-character.component.ts
+++ src/app/home/components/header-character/header-character.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import Parallax from 'parallax-js';
 @Component({
   selector: 'app-header-character',
   templateUrl: './header-character.component.html',
-  styleUrls: ['./header-character.component.scss']
+  styleUrls: ['./header-character.component.scss'],
 })
-export class HeaderCharacterComponent implements OnInit {
+export class HeaderCharacterComponent implements AfterViewInit {
+  @ViewChild('parallaxScene') parallaxScene!: ElementRef<HTMLDivElement>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    new Parallax(this.parallaxScene.nativeElement, {
+      selector: '.parallax-layer',
+    });
   }
-
 }
--- src/app/shared/components/bingo-games/bingo-games.component.ts
+++ src/app/shared/components/bingo-games/bingo-games.component.ts
@@ -1,17 +1,32 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { BingoGame } from '../../models/bingo-game';
 import { CarouselService } from '../../services/carousel.service';
+import Parallax from 'parallax-js';
 
 @Component({
   selector: 'app-bingo-games',
   templateUrl: './bingo-games.component.html',
   styleUrls: ['./bingo-games.component.scss'],
 })
-export class BingoGamesComponent {
+export class BingoGamesComponent implements AfterViewInit {
+  @ViewChild('parallaxScene') parallaxScene!: ElementRef<HTMLDivElement>;
+
   @Input() games: BingoGame[] = [];
   @Input() showInCarousel = true;
 
   carouselOptions = this.carouselService.getGamesCarouselOptions(4);
 
   constructor(private carouselService: CarouselService) {}
+
+  ngAfterViewInit(): void {
+    new Parallax(this.parallaxScene.nativeElement, {
+      selector: '.parallax-layer',
+    });
+  }
 }
