9a174fa54985ed130a4d3c97a7a67a4576ba0256
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-06-30 21:50:21 +0300

    parallax fix for casino header

--- src/app/bingo/components/bingo-new-games/bingo-new-games.component.ts
+++ src/app/bingo/components/bingo-new-games/bingo-new-games.component.ts
@@ -1,5 +1,12 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import Parallax from 'parallax-js';
 import { BingoGame } from 'src/app/shared/models/bingo-game';
 import { CarouselService } from 'src/app/shared/services/carousel.service';
 import { GamesService } from 'src/app/shared/services/games.service';
@@ -9,7 +16,8 @@ import { GamesService } from 'src/app/shared/services/games.service';
   templateUrl: './bingo-new-games.component.html',
   styleUrls: ['./bingo-new-games.component.scss'],
 })
-export class BingoNewGamesComponent {
+export class BingoNewGamesComponent implements AfterViewInit {
+  @ViewChild('parallaxScene') parallaxScene!: ElementRef<HTMLDivElement>;
   @Input() games: BingoGame[] | null = [];
   carouselOptions: OwlOptions = this.carouselService.newGamesCarouselOptions;
 
@@ -17,6 +25,12 @@ export class BingoNewGamesComponent {
     private gamesService: GamesService,
     private carouselService: CarouselService
   ) {}
+
+  ngAfterViewInit(): void {
+    new Parallax(this.parallaxScene.nativeElement, {
+      selector: '.parallax-layer',
+    });
+  }
 
   showBingoGameDetails(game: BingoGame) {
     this.gamesService.showBingoGameDetails(game);
