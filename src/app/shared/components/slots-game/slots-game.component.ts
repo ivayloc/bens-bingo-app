496678f6fa2f0fb9564e4e10632a67f7877c4b02
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-07-01 16:47:57 +0300

    slots games fix

--- src/app/shared/components/slots-game/slots-game.component.ts
+++ src/app/shared/components/slots-game/slots-game.component.ts
@@ -1,14 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SlotsGame } from '../../models/slots-game';
+import { GamesService } from '../../services/games.service';
 
 @Component({
   selector: 'app-slots-game',
   templateUrl: './slots-game.component.html',
   styleUrls: ['./slots-game.component.scss'],
 })
-export class SlotsGameComponent implements OnInit {
+export class SlotsGameComponent {
   @Input() game = {} as SlotsGame;
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor(private gamesService: GamesService) {}
+
+  showBingoGameDetails(game: SlotsGame) {
+    this.gamesService.showBingoGameDetails(game);
+  }
 }
