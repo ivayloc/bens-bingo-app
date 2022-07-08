f82b553cb9e611c5b04100d43f1bfdc31c552359
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-07-09 01:31:20 +0300

    bonus

--- src/app/cashier/components/bonus-code/bonus-code.component.ts
+++ src/app/cashier/components/bonus-code/bonus-code.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-bonus-code',
   templateUrl: './bonus-code.component.html',
-  styleUrls: ['./bonus-code.component.scss']
+  styleUrls: ['./bonus-code.component.scss'],
 })
 export class BonusCodeComponent implements OnInit {
+  bonusCodeField = this.fb.control('');
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
