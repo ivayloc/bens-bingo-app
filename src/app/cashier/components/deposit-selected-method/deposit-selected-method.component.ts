6e03cf22c0faa1cb0fe6d2bb4adb2306898d8102
Ivaylo Cherkezov <ivaylo.cherkezov@taryafintech.com>
2022-08-18 00:58:47 +0300

    choose card in progress

--- src/app/cashier/components/deposit-selected-method/deposit-selected-method.component.ts
+++ src/app/cashier/components/deposit-selected-method/deposit-selected-method.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { PaymentMethod } from '../../models/payment-method';
-import { selectSelectedDepositMethod } from '../../state';
+import { PaymentMethodAccount } from '../../models/payment-method-account';
+import {
+  selectSelectedDepositMethod,
+  selectSelectedDepositMethodAccountMatData,
+} from '../../state';
 
 @Component({
   selector: 'app-deposit-selected-method',
@@ -10,13 +16,46 @@ import { selectSelectedDepositMethod } from '../../state';
   styleUrls: ['./deposit-selected-method.component.scss'],
 })
 export class DepositSelectedMethodComponent implements OnInit {
-  getSelectedDepositMethod$ = new Observable<PaymentMethod>();
+  showCardsStep = false;
+  depositForm = this.fb.group({
+    amount: ['', Validators.required],
+  });
+  getSelectedDepositMethodAccounts$ = new Observable<
+    MatTableDataSource<PaymentMethodAccount>
+  >();
 
-  constructor(private store: Store) {}
+  public get depositAmountField(): FormControl {
+    return this.depositForm.get('amount') as FormControl;
+  }
+
+  getSelectedDepositMethod$ = new Observable<PaymentMethod>();
+  displayedColumns = [
+    'type',
+    'number',
+    'nameOnCard',
+    'Expiry',
+    'type',
+    'action',
+    'transactionid',
+    'result',
+  ];
+  constructor(private store: Store, private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.getSelectedDepositMethod$ = this.store.select(
-      selectSelectedDepositMethod
+    this.getSelectedDepositMethod$ = this.store
+      .select(selectSelectedDepositMethod)
+      .pipe(
+        tap((paymentMethod) => {
+          this.depositAmountField.setValue(paymentMethod?.default);
+        })
+      );
+
+    this.getSelectedDepositMethodAccounts$ = this.store.select(
+      selectSelectedDepositMethodAccountMatData
     );
+  }
+
+  continue() {
+    this.showCardsStep = true;
   }
 }
